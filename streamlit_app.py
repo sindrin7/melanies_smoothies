a9e2fea8b109be8cfa1ff805a04798969a2da9f6
Jing Qian <qj883107@hotmail.com>
2025-03-16 07:51:38 -0700

    Update streamlit_app.py
    Added conditional fruit 'address'

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,14 @@ if ingredients_list:
         
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        if search_on:
+            address = search_on
+        else:
+            address = fruit_chosen
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + address)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     my_insert_stmt = """ 
